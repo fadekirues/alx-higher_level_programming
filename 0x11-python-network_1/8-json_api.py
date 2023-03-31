80948ae233c176bf4ff3cc6e96e5c5027dcdd581
fadekirues <fdkiruegbukpe@gmail.com>
2023-03-31 16:49:11 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 """
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter
+Module  sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    if len(argv) == 2:
-        q = argv[1]
-    else:
+import requests
+from sys import argv
+
+
+def main(argv):
+    """
+    Script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+    """
+    if len(argv) < 2:
         q = ""
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    else:
+        q = argv[1]
+    payload = {'q': q}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
     try:
-        r_dict = r.json()
-        id = r_dict.get('id')
-        name = r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
+        result = r.json()
+        if bool(result) is False:
             print("No result")
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+            print("[{}] {}".format(result['id'], result['name']))
     except:
         print("Not a valid JSON")
+
+if __name__ == "__main__":
+    main(argv)
